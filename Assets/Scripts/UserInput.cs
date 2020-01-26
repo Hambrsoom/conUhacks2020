8b31840a568c17fdfd92f2c271296e2b87fb7aef
Hambrsoom <hampico1234@gmail.com>
2020-01-26 02:15:34 -0500

    user input

---
+++ Assets/Scripts/UserInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UserInput : MonoBehaviour
+{
+    private string input;
+    public GameObject inputField;
+
+    public void saveInput()
+    {
+        input = inputField.GetComponent<Text>().text;
+    }
+}
